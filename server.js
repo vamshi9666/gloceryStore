5385c7d4ca1ccb78169378eb9915e0c695462a77
vamshikn <vamshikn@upscalesoft.in>
2018-06-17 20:52:03 +0530

    data in expected format

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-
-request('http://manarythubazar.com/Telangana/Hyderabad/Mehdipatnam',(err, res, body)=>{
+var url = "http://manarythubazar.com/Telangana/Hyderabad/Mehdipatnam";
+request(url,(err, res, body)=>{
 	if(!err){
-		cheerio.load(body);
-		let re = $('.col-md-4').text();
-		console.log(re)
+		var $ = cheerio.load(body);
+		let re = $('.row').children();
+
+
+		var prices = [];
+		re.map(index=>{
+			prices.push($('ol').children().eq(index).find("td").text());
+		})
+		console.log(prices)
+		
 	}
 })
