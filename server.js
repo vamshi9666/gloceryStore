b28d1acbc7c8e3cf341d0759bc940ebdac0bc4df
vamshikn <vamshikn@upscalesoft.in>
2018-06-19 22:46:58 +0530

    able to get data in arrays in arrays but need to modify to push to a super array

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+const mongoose = require('mongoose');
+const  dotenv = require('dotenv').config();
+const conn = mongoose.createConnection(process.env.DB_LINK);
+
+
+conn.on('connected', ()=>{
+	console.log("Db connected !");
+})
+conn.on('disconnected', ()=>{
+	console.log("DB disconnected !");
+})
+
+// const connection = mongoose.connection();
+//
+// connection.on('open',()=>{
+// 	console.log("DB connection opened !")
+// })
+//
+// connection.on('error',()=>{
+// 	console.log("error opening Database !");
+// })
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
@@ -6,7 +27,9 @@ request(url,(err, res, body)=>{
 	if(!err){
 		var $ = cheerio.load(body);
 		let re = $('.col-lg-12').children();
-
+		if(err){
+			console.log(err)
+		}
 
 		var prices = [];
 		re.map(index=>{
@@ -14,8 +37,13 @@ request(url,(err, res, body)=>{
 		})
 		console.log(prices);
 		for (price of prices){
-			console.log(price)
+			// const spilt_price = price.split("₹")
+			// // prices.push(spilt_price)
+			// 	console.log(price.split("₹"));
+				const price_final =  price.split("₹");
+				console.log(price_final);
+				prices.push(price_final)
 		}
-		
+
 	}
 })
