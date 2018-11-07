ddfd355598ded43b5ca5c941cfd4347391577c25
vamshi9666 <vamshi9666@riseup.net>
2018-11-07 16:59:25 +0530

    getting rates from mehidipatm=nam

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
-const mongoose = require('mongoose');
-const  dotenv = require('dotenv').config();
-const conn = mongoose.createConnection(process.env.DB_LINK);
-
-
-conn.on('connected', ()=>{
-	console.log("Db connected !");
-})
-conn.on('disconnected', ()=>{
-	console.log("DB disconnected !");
-})
-
-// const connection = mongoose.connection();
-//
-// connection.on('open',()=>{
-// 	console.log("DB connection opened !")
-// })
-//
-// connection.on('error',()=>{
-// 	console.log("error opening Database !");
-// })
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-var url = "http://manarythubazar.com/Telangana/Hyderabad/Mehdipatnam";
-request(url,(err, res, body)=>{
-	if(!err){
-		var $ = cheerio.load(body);
-		let re = $('.col-lg-12').children();
+
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res,next)=> {
+		var url = "http://manarythubazar.com/Telangana/Hyderabad/Mehdipatnam";
+		request(url,(err, data, body)=>{
+		if(!err){
+			var $ = cheerio.load(body);
+			let re = $('.col-lg-12').children();
 		if(err){
 			console.log(err)
+			res.status(400).json({
+				message:'badRequest'
+			})
 		}
 
 		var prices = [];
@@ -36,14 +24,14 @@ request(url,(err, res, body)=>{
 			prices.push($('ol').children().eq(index).find("td").text());
 		})
 		console.log(prices);
-		for (price of prices){
-			// const spilt_price = price.split("₹")
-			// // prices.push(spilt_price)
-			// 	console.log(price.split("₹"));
-				const price_final =  price.split("₹");
-				console.log(price_final);
-				prices.push(price_final)
-		}
+		res.status(200).json(prices)
 
 	}
+})
+
+})
+
+
+app.listen(1000, () => {
+	console.log(`Server listenign on port 1000`)
 })
